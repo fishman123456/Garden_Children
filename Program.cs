f9923ee2c020036dbd808ccb7335675697638bdb
denis <106389581+fishman123456@users.noreply.github.com>
2022-10-19 08:25:14 +0700

    19-10-2022 На работе
    Добавлено переприсваивание,

--- Program.cs
+++ Program.cs
@@ -10,17 +10,27 @@
 // сада и какой процент.Также сделать функцию вывода информации 
 // о детском саде. Создать три детских сада.
 
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.SetWindowSize(100, 30);
-Console.WriteLine("Начало программы детские сады и специализация");
+        Console.Title = "******";
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.SetWindowSize(100, 30);
+        Console.WriteLine("Начало программы детские сады и специализация");
+    
 
-
-class garden              // Создаем класс  детский сад
-{
-    private string _name; // поле имя детского сада
-    private string _adress; // поле адрес детского сада
-    private string _description; // поле пояснение к детскому саду (специализация)
-
-
+    class garden              // Создаем класс  детский сад
+    {
+        private string _name; // поле имя детского сада
+        private string _adress; // поле адрес детского сада
+        private string _description; // поле пояснение к детскому саду (специализация)
+        private string _count_child; // количество детей
+        public static string _max_count_child; // максимальное кол-во детей
+    
+    public garden( string name, string adress, string description, string count_child, string max_count_child )
+{ 
+        this._name = name;
+        this._adress = adress;
+        this._description = description;
+        this._count_child = count_child; 
+    }
 }
+
